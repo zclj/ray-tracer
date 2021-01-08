2e6d1296314cb172469fe0efb2764712d867ccd6
Stefan Karlsson <info-s@tryne.se>
2021-01-08 11:34:29 +0100

    First Tuple and point version

---
+++ haskell/src/Tuples.hs
@@ -0,0 +1,17 @@
+module Tuples where
+
+data Tuple = Tuple { x :: Double
+                   , y :: Double
+                   , z :: Double
+                   , w :: Double}
+             deriving (Show, Eq)
+
+point x y z = Tuple x y z 1.0
+
+isPoint :: Tuple -> Bool
+isPoint (Tuple _ _ _ w)
+  | w == 1    = True
+  | otherwise = False
+
+
+{- https://www.youtube.com/watch?v=6COvD8oynmI 35 -}
