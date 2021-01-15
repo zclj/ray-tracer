b81057161a3923412a8c41a7d1dc031d8c4a3597
Stefan Karlsson <info-s@tryne.se>
2021-01-15 09:57:57 +0100

    Added type sig. to point and vector

--- haskell/src/Tuples.hs
+++ haskell/src/Tuples.hs
@@ -6,11 +6,13 @@ data Tuple = Tuple { x :: Double
                    , w :: Double}
              deriving (Show, Eq)
 
+point :: Double -> Double -> Double -> Tuple
 point x y z = Tuple x y z 1.0
 
 isPoint :: Tuple -> Bool
 isPoint (Tuple _ _ _ w) = w == 1
 
+vector :: Double -> Double -> Double -> Tuple
 vector x y z = Tuple x y z 0.0
 
 isVector :: Tuple -> Bool
