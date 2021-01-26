8869232a8e07c0255047c89eeecbda264956dd80
Stefan Karlsson <info-s@tryne.se>
2021-01-26 14:58:22 +0100

    Prepared for Canvas

---
+++ haskell/test/CanvasSpec.hs
@@ -0,0 +1,23 @@
+module CanvasSpec where
+
+import System.IO.Unsafe (unsafePerformIO)
+
+import Test.Tasty
+import Test.Tasty.Hspec as HS
+import Canvas as SUT
+
+canvasTests :: TestTree
+canvasTests = testGroup "Canvas Tests" [
+  testGroup "HSpec tests"
+  [ unsafePerformIO (testSpec "Canvas" canvasBasics)]]
+
+canvasBasics :: Spec
+canvasBasics =
+  describe "Basics" $ do
+    {-  -}
+    describe "A canvas .." $ do
+      it "returns x" $ do
+        pendingWith "Implementation"
+
+
+--pendingWith "Implementation"
