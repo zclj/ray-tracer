f53e0827ae4c3ad09c22a310b4b65c75fd7075d1
Stefan Karlsson <info-s@tryne.se>
2021-01-27 11:34:24 +0100

    Experimenting with Canvas creation

--- haskell/src/Canvas.hs
+++ haskell/src/Canvas.hs
@@ -1,1 +1,17 @@
 module Canvas where
+
+newtype Width  = Width Double
+  deriving (Show, Eq)
+
+newtype Height = Height Double
+  deriving (Show, Eq)
+
+data Canvas = Canvas { width :: Width
+                     , height :: Height }
+              deriving(Show, Eq)
+
+row :: Int -> [[Int]]
+row x = [[0] | x <- [1..x]]
+
+canvas :: Int -> Int -> [[[Int]]]
+canvas x y = (foldr (\_ canvas -> row x : canvas) [] [1..y])
--- haskell/test/CanvasSpec.hs
+++ haskell/test/CanvasSpec.hs
@@ -14,10 +14,19 @@ canvasTests = testGroup "Canvas Tests" [
 canvasBasics :: Spec
 canvasBasics =
   describe "Basics" $ do
-    {-  -}
-    describe "A canvas .." $ do
-      it "returns x" $ do
-        pendingWith "Implementation"
+    {-  Scenario: Creating a canvas
+          Given c ← canvas(10, 20)
+          Then c.width = 10
+            And c.height = 20
+            And every pixel of c is color(0, 0, 0) -}
+    describe "Creating" $ do
+      --it "creates a canvas with all black pixels" $ do
+      let c = Canvas (Width 10) (Height 20)
+      it "returns width" $ do
+        (width c) `shouldBe` (Width 10)
+
+      it "returns height" $ do
+        (height c) `shouldBe` (Height 20)
 
 
 --pendingWith "Implementation"
