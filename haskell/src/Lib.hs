edebd5df1d5e6e6901036e2c466dc138791b0428
Stefan Karlsson <info-s@tryne.se>
2021-11-03 15:54:04 +0100

    Add function to write canvas to file

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -1,6 +1,18 @@
 module Lib
     ( someFunc
+    , demoCanvas
+    , writeCanvas
     ) where
 
-someFunc :: IO ()
+import Canvas
+import Tuples
+
 someFunc = putStrLn "someFunc"
+
+demoCanvas =  let writeRow = (\w -> [Color (Red 1) (Green 0.8) (Blue 0.6) | _ <- [1..100]])
+                  coloredCanvas = foldr (\_ canvas -> writeRow w : canvas) [] [1..20]
+                  ppm           = unlines (canvasToPPM coloredCanvas)
+              in ppm
+
+writeCanvas :: String -> IO ()
+writeCanvas c = writeFile "canvas.ppm" c
