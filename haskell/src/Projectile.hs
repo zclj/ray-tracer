ee9eaea1e3dfd9d55b7cad3087ae9bbb115ff6fc
Stefan Karlsson <info-s@tryne.se>
2022-01-31 12:26:10 +0100

    Fix long lines

--- haskell/src/Projectile.hs
+++ haskell/src/Projectile.hs
@@ -42,9 +42,10 @@ launch' env p@(Projectile (Tuple _ y _ _) _) xs
   | otherwise = p : launch' env (tick env p) xs
 
 launch :: Gravity -> Wind -> [Projectile]
-launch (Gravity g) (Wind w) = let env  = Environment (vector 0 g 0) (vector w 0 0) 
-                                  proj = Projectile (point 0 1 0) ((norm (vector 1 1.8 0)) `mul` 11.25)
-                              in launch' env proj []
+launch (Gravity g) (Wind w) =
+  let env  = Environment (vector 0 g 0) (vector w 0 0) 
+      proj = Projectile (point 0 1 0) ((norm (vector 1 1.8 0)) `mul` 11.25)
+  in launch' env proj []
                                                   
 launchresult = let g = (Gravity (-0.1))
                    w = (Wind (-0.01))
