2db360675ceea1d8c41980ba87f5fbbd7f86daa6
Stefan Karlsson <info-s@tryne.se>
2022-02-01 07:07:26 +0100

    Remove redundant brackets

--- haskell/src/Tuples.hs
+++ haskell/src/Tuples.hs
@@ -13,7 +13,7 @@ instance Eq Tuple where
   (Tuple x1 y1 z1 w1) == (Tuple x2 y2 z2 w2)
     = let epsilon = 0.0001
           ltep    = (\x y -> abs (x - y) < epsilon)
-      in (ltep x1 x2) && (ltep y1 y2) && (ltep z1 z2) && (ltep w1 w2)  
+      in ltep x1 x2 && ltep y1 y2 && ltep z1 z2 && ltep w1 w2  
 
 point :: Double -> Double -> Double -> Tuple
 point x y z = Tuple x y z 1.0
@@ -36,7 +36,7 @@ sub (Tuple x1 y1 z1 w1) (Tuple x2 y2 z2 w2)
   = Tuple (x1 - x2) (y1 - y2) (z1 - z2) (w1 - w2)
 
 neg :: Tuple -> Tuple
-neg (Tuple x y z w) = (Tuple (-x) (-y) (-z) (-w))
+neg (Tuple x y z w) = Tuple (-x) (-y) (-z) (-w)
 
 mul :: Tuple -> Double -> Tuple
 mul (Tuple x y z w) s = Tuple (x * s) (y * s) (z * s) (w * s)
@@ -48,7 +48,7 @@ mag :: Tuple -> Double
 mag (Tuple x y z w) = sqrt $ x^2 + y^2 + z^2 + w^2
 
 norm :: Tuple -> Tuple
-norm t@(Tuple x y z w) = let m = (mag t)
+norm t@(Tuple x y z w) = let m = mag t
                          in Tuple (x / m) (y / m) (z / m) (w / m)
 
 dot :: Tuple -> Tuple -> Double
@@ -105,7 +105,7 @@ mulCS (Color (Red r1) (Green g1) (Blue b1)) x =
   
 mulC :: Color -> Color -> Color
 mulC (Color (Red r1) (Green g1) (Blue b1)) (Color (Red r2) (Green g2) (Blue b2)) =
-  let r = (Red (r1 * r2))
-      g = (Green (g1 * g2))
-      b = (Blue (b1 * b2))
+  let r = Red (r1 * r2)
+      g = Green (g1 * g2)
+      b = Blue (b1 * b2)
   in Color r g b
