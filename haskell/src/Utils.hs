d4cdd7902185547c90aa7582fed4d3745d88f596
Stefan Karlsson <info-s@tryne.se>
2022-02-11 08:19:44 +0100

    Use concat instead of ++ (slight perf. improvement 1.43 -> 1.36)

--- haskell/src/Utils.hs
+++ haskell/src/Utils.hs
@@ -8,9 +8,7 @@ module Utils
   ) where
 
 addToLast :: a -> [[a]] -> [[a]]
-addToLast x xs = pre ++ [post ++ [x]]
-  where pre  = init xs
-        post = last xs
+addToLast x xs = concat [init xs, [concat [last xs, [x]]]]
 
 whenSumOf :: (Num b, Ord b) => (b -> Bool) -> (a -> b) -> [a] -> Bool
 whenSumOf p f l = p $ sum (map f l)
