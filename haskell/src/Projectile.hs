5ad4cdd320b7d22d954943f82d8aeb793e96ca91
Stefan Karlsson <info-s@tryne.se>
2022-03-09 07:46:22 +0100

    Do not use # in comment, linter thinks its a directive

--- haskell/src/Projectile.hs
+++ haskell/src/Projectile.hs
@@ -3,11 +3,11 @@ module Projectile where
 import Tuples
 
 {-
-# projectile starts one unit above the origin.
-# velocity is normalized to 1 unit/tick.
+- projectile starts one unit above the origin.
+- velocity is normalized to 1 unit/tick.
 p ← projectile(point(0, 1, 0), normalize(vector(1, 1, 0)))
 
-# gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
+- gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
 e ← environment(vector(0, -0.1, 0), vector(-0.01, 0, 0))
 
 function tick(env, proj)
