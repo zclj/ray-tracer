0d4933587a7e45acdf4a2b9c9d112576480d874f
Stefan Karlsson <info-s@tryne.se>
2022-03-11 07:24:21 +0100

    Make Matrix generic

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -1,8 +1,9 @@
 module Matrices where
 
-data Matrix = Matrix [[Double]]
+data Matrix a = Matrix [[a]]
   deriving (Show, Eq)
 
+makeMatrix :: [[a]] -> Matrix a
 makeMatrix [ [a11, a12, a13, a14]
            , [a21, a22, a23, a24]
            , [a31, a32, a33, a34]
