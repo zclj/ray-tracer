bd8487bea311663b5d3787be4b71ada94f8fd163
Stefan Karlsson <info-s@tryne.se>
2022-03-11 07:24:42 +0100

    Add implementation stubb for 'getAt' in Matrices

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -12,5 +12,13 @@ makeMatrix [ [a11, a12, a13, a14]
            , [a21, a22, a23, a24]
            , [a31, a32, a33, a34]
            , [a41, a42, a43, a44]]
-
 makeMatrix _ = error "Unsupported Matrix size"
+
+newtype RowIndex = RowIndex Int
+  deriving (Show, Eq, Ord)
+
+newtype ColumnIndex = ColumnIndex Int
+  deriving (Show, Eq, Ord)
+
+getAt :: Matrix a -> RowIndex -> ColumnIndex -> a
+getAt m r a = undefined
