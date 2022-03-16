46b1b118296a1ecb7be91b98cbe16fa35083ef5f
Stefan Karlsson <info-s@tryne.se>
2022-03-16 08:15:44 +0100

    Add basic implementation of Matrix multiplication

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -49,5 +49,13 @@ newtype ColumnIndex = ColumnIndex Int
 getAt :: Matrix a -> RowIndex -> ColumnIndex -> a
 getAt (Matrix m) (RowIndex r) (ColumnIndex c) = (m !! r) !! c
 
-mul :: Matrix a -> Matrix a -> Matrix a
-mul (Matrix a) (Matrix b) = undefined
+mul :: Num a => Matrix a -> Matrix a -> Matrix a
+mul (Matrix a) (Matrix b) = Matrix [ [((getAt (Matrix a) (RowIndex i) (ColumnIndex 0)) *
+                                       (getAt (Matrix b) (RowIndex 0) (ColumnIndex j))) +
+                                      ((getAt (Matrix a) (RowIndex i) (ColumnIndex 1)) *
+                                       (getAt (Matrix b) (RowIndex 1) (ColumnIndex j))) +
+                                      ((getAt (Matrix a) (RowIndex i) (ColumnIndex 2)) *
+                                       (getAt (Matrix b) (RowIndex 2) (ColumnIndex j))) +
+                                      ((getAt (Matrix a) (RowIndex i) (ColumnIndex 3)) *
+                                       (getAt (Matrix b) (RowIndex 3) (ColumnIndex j)))
+                                     | j <- [0..3]] | i <- [0..3]]
