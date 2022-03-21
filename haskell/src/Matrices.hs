f0951e5e0177706a32a0d2eac762fbee5a3d2371
Stefan Karlsson <info-s@tryne.se>
2022-03-21 07:11:23 +0100

    Remove redundant brackets

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -61,6 +61,6 @@ mul a b =
 mulT :: Matrix Double -> T.Tuple -> T.Tuple
 mulT a@(Matrix m) b = let get = (\m r c -> getAt m (RowIndex r) (ColumnIndex c))
                           tupleFromList [x, y, z, w] = T.Tuple x y z w
-                      in tupleFromList [(tupleFromList (m !! i)) `T.dot` b
+                      in tupleFromList [tupleFromList (m !! i) `T.dot` b
                                        | i <- [0..3]]
 
