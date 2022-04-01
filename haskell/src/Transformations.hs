5bbd84eeab0d6357c23b5458e27d821b75194ab7
Stefan Karlsson <info-s@tryne.se>
2022-04-01 07:54:56 +0200

    Return a translation matrix

--- haskell/src/Transformations.hs
+++ haskell/src/Transformations.hs
@@ -10,5 +10,7 @@ import Matrices
 transform = undefined
 
 translation :: Double -> Double -> Double -> Matrix Double
-translation = undefined
-
+translation x y z = makeMatrix [[1, 0, 0, x],
+                                [0, 1, 0, y],
+                                [0, 0, 1, z],
+                                [0, 0, 0, 1]]
