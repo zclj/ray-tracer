71457a700c3d9310bc2e1c757e1fc81e22a7073c
Stefan Karlsson <info-s@tryne.se>
2022-04-04 07:16:18 +0200

    Add the scaling transformation matrix

--- haskell/src/Transformations.hs
+++ haskell/src/Transformations.hs
@@ -11,4 +11,8 @@ translation x y z = makeMatrix [[1, 0, 0, x],
                                 [0, 0, 1, z],
                                 [0, 0, 0, 1]]
 
-scaling x y z = undefined
+scaling :: Double -> Double -> Double -> Matrix Double
+scaling x y z = makeMatrix [[x, 0, 0, 0],
+                            [0, y, 0, 0],
+                            [0, 0, z, 0],
+                            [0, 0, 0, 1]]
