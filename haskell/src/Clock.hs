100c9e71e22ef7cc231e2b812763ed7443633174
Stefan Karlsson <info-s@tryne.se>
2022-04-20 07:21:25 +0200

    Make a matrix transformation-based clock

--- haskell/src/Clock.hs
+++ haskell/src/Clock.hs
@@ -12,17 +12,14 @@ twelve = point 0 0 1
 
 clockWidth = 800 * (3/8)
 
-toScreen :: Tuple -> Tuple
-toScreen p = let scaled = point
-                          ((x p) * clockWidth)
-                          (y p)
-                          ((z p) * clockWidth)
-                 moved  = scaled `add` (point 400 400 400)
-                 tilted = point (x moved) (z moved) 0
-             in tilted
+hourTransform :: Double -> Matrix
+hourTransform i = transform [rotationY (i * (pi/6)),
+                             scaling clockWidth 0 clockWidth,
+                             translation 400 0 400,
+                             rotationX ((3/2) * pi)]
 
-hours = foldr (\i points -> (mulT (rotationY (i * (pi/6))) twelve) : points)  [] [1..12]
+hours :: [Tuple]
+hours = map (\i -> mulT (hourTransform i) twelve) [1..12]
 
-screenHours = map toScreen hours
-
-clock = screenHours
+clock :: [Tuple]
+clock = hours
