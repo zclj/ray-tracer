696ada032f576d60239ae95de047bd497259d680
Stefan Karlsson <info-s@tryne.se>
2022-04-21 07:40:42 +0200

    Add link to line-sphere intersection article

--- haskell/src/Spheres.hs
+++ haskell/src/Spheres.hs
@@ -13,5 +13,6 @@ data Sphere = Sphere { id     :: Int
 makeUnitSphere :: Int -> Sphere
 makeUnitSphere id = Sphere id 1.0
 
+-- https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 intersects :: Sphere -> Ray -> [Double]
 intersects s r = undefined
