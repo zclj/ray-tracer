64d08e573ac56eaea45054dc5df9844773079ef9
Stefan Karlsson <info-s@tryne.se>
2022-04-22 07:33:59 +0200

    Add test for ray originating inside a sphere

--- haskell/test/SpheresSpec.hs
+++ haskell/test/SpheresSpec.hs
@@ -62,3 +62,20 @@ sphereIntersections =
           xs = SUT.intersects s r
       it "there are no intersections" $ do
         length xs `shouldBe` 0
+    {- Scenario: A ray originates inside a sphere
+         Given r ← ray(point(0, 0, 0), vector(0, 0, 1))
+           And s ← sphere()
+         When xs ← intersect(s, r)
+         Then xs.count = 2
+           And xs[0] = -1.0
+           And xs[1] = 1.0 -}
+    describe "A ray originates inside a sphere" $ do
+      let r = makeRay (point 0 0 0) (vector 0 0 1)
+          s = SUT.makeUnitSphere 1
+          xs = SUT.intersects s r
+      it "there are two intersections" $ do
+        length xs `shouldBe` 2
+      it "intersection one is -1.0" $ do
+        (head xs) `shouldBe` (-1.0)
+      it "intersection two is 1.0" $ do
+        (last xs) `shouldBe` 1.0
