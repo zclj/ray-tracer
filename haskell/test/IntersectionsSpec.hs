a6735dc5d88c0aa622462f5ceb552a2e5610c345
Stefan Karlsson <info-s@tryne.se>
2022-04-25 07:57:44 +0200

    Add test for aggregation of intersections

--- haskell/test/IntersectionsSpec.hs
+++ haskell/test/IntersectionsSpec.hs
@@ -28,3 +28,24 @@ intersections =
         (t i) `shouldBe` 3.5
       it "object is the sphere" $ do
         (object i) `shouldBe` s
+    {- Scenario: Aggregating intersections
+         Given s ← sphere()
+           And i1 ← intersection(1, s)
+           And i2 ← intersection(2, s)
+         When xs ← intersections(i1, i2)
+         Then xs.count = 2
+           And xs[0].t = 1
+           And xs[1].t = 2 -}
+    describe "Aggregating intersections" $ do
+      let s = makeUnitSphere 1
+          i1 = SUT.Intersection 1 s
+          i2 = SUT.Intersection 2 s
+          -- Feature says to use a type 'Intersections' but it seems to be just a
+          -- list for now. Change if needed
+          xs = [i1, i2]
+      it "the number of intersections is 2" $ do
+        length xs `shouldBe` 2
+      it "the first intersections t is 1" $ do
+        t (head xs) `shouldBe` 1
+      it "the second intersections t is 2" $ do
+        t (last xs) `shouldBe` 2
