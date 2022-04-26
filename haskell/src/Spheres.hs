0848bc798afcb5a7c0b2fd9a0c46666f7dafdce2
Stefan Karlsson <info-s@tryne.se>
2022-04-26 07:10:36 +0200

    Remove 'intersects' from Sphere

--- haskell/src/Spheres.hs
+++ haskell/src/Spheres.hs
@@ -1,6 +1,5 @@
 module Spheres
   ( Sphere (..)
-  , intersects
   , makeUnitSphere
   ) where
 
@@ -15,13 +14,3 @@ data Sphere = Sphere { id     :: Int
 makeUnitSphere :: Int -> Sphere
 makeUnitSphere id = Sphere id 1.0
 
-intersects :: Sphere -> Ray -> [Double]
-intersects s r = let sphereToRay  = (origin r) `sub` point 0 0 0
-                     a            = (direction r) `dot` (direction r)
-                     b            = 2 * ((direction r) `dot` sphereToRay)
-                     c            = (sphereToRay `dot` sphereToRay) - 1
-                     discriminant = b^2 - (4 * a * c)
-                 in if discriminant < 0
-                    then []
-                    else [ ((-b) - (sqrt discriminant)) / (2 * a)
-                         , ((-b) + (sqrt discriminant)) / (2 * a)]
