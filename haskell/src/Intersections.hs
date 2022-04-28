5ec4871d2349b163b4afda6523f7f913ca61b23b
Stefan Karlsson <info-s@tryne.se>
2022-04-28 07:49:10 +0200

    Remove redundant brackets

--- haskell/src/Intersections.hs
+++ haskell/src/Intersections.hs
@@ -14,17 +14,17 @@ data Intersection = Intersection { t      :: Double
                   deriving (Show, Eq, Ord)
 
 intersect :: Sphere -> Ray -> [Intersection]
-intersect s r = let sphereToRay  = (origin r) `sub` point 0 0 0
-                    a            = (direction r) `dot` (direction r)
-                    b            = 2 * ((direction r) `dot` sphereToRay)
+intersect s r = let sphereToRay  = origin r `sub` point 0 0 0
+                    a            = direction r `dot` direction r
+                    b            = 2 * (direction r `dot` sphereToRay)
                     c            = (sphereToRay `dot` sphereToRay) - 1
                     discriminant = b^2 - (4 * a * c)
                 in if discriminant < 0
                    then []
-                   else [ Intersection (((-b) - (sqrt discriminant)) / (2 * a)) s
-                        , Intersection (((-b) + (sqrt discriminant)) / (2 * a)) s]
+                   else [ Intersection (((-b) - sqrt discriminant) / (2 * a)) s
+                        , Intersection (((-b) + sqrt discriminant) / (2 * a)) s]
 
 -- |The `hit` function returns the first non-negative intersection.
 -- Intersections with a negative value are 'behind', positive 'infront'
 hit :: [Intersection] -> Maybe Intersection
-hit xs = find (\(Intersection t _) -> (t > 0)) $ sort xs
+hit xs = find (\(Intersection t _) -> t > 0) $ sort xs
