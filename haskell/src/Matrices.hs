82496e1955e407b5bb79b93926e39df3849f0cdd
Stefan Karlsson <info-s@tryne.se>
2022-05-09 07:59:33 +0200

    Play around with specializing matrix operations

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -1,6 +1,8 @@
 module Matrices
   ( Matrix
   , makeMatrix
+  , makeMatrix4x4
+  , makeMatrix3x3
   , getAt
   , RowIndex (..)
   , ColumnIndex (..)
@@ -58,6 +60,31 @@ makeMatrix [ [a11, a12]
            , [a21, a22]]
 makeMatrix _ = error "Unsupported Matrix size"
 
+----
+-- Specialized matrix
+
+data Matrix4x4 = Matrix4x4 [[Double]]
+
+makeMatrix4x4 [ [a11, a12, a13, a14]
+              , [a21, a22, a23, a24]
+              , [a31, a32, a33, a34]
+              , [a41, a42, a43, a44]]
+  = Matrix4x4 [ [a11, a12, a13, a14]
+              , [a21, a22, a23, a24]
+              , [a31, a32, a33, a34]
+              , [a41, a42, a43, a44]]
+
+data Matrix3x3 = Matrix3x3 [[Double]]
+
+makeMatrix3x3 [ [a11, a12, a13]
+              , [a21, a22, a23]
+              , [a31, a32, a33]]
+  = Matrix3x3 [ [a11, a12, a13]
+              , [a21, a22, a23]
+              , [a31, a32, a33]]
+
+----
+
 identity :: Matrix
 identity = Matrix [[1, 0, 0, 0], [0, 1, 0, 0],
                    [0, 0, 1, 0], [0, 0, 0, 1]]
@@ -93,11 +120,49 @@ determinant m@(Matrix x) =
       size      = length (head x) - 1
   in sum $ map (\j -> onRowZero getAt j * onRowZero cofactor j) [0..size]
 
+-- submatrix :: Matrix -> RowIndex -> ColumnIndex -> Matrix
+-- submatrix (Matrix a) (RowIndex r) (ColumnIndex c)
+--   = let subRows = dropAt r a
+--         subCols = map (dropAt c) subRows
+--     in Matrix subCols
+
+subColsX :: Int -> Matrix -> Matrix
+subColsX i
+  (Matrix [[x1,y1,z1,w1], [x2,y2,z2,w2], [x3,y3,z3,w3]])
+  = let xs = case i of
+               0 -> [[y1,z1,w1]
+                    ,[y2,z2,w2]
+                    ,[y3,z3,w3]]
+               1 -> [[x1,z1,w1]
+                    ,[x2,z2,w2]
+                    ,[x3,z3,w3]]
+               2 -> [[x1,y1,w1]
+                    ,[x2,y2,w2]
+                    ,[x3,y3,w3]]
+               3 -> [[x1,y1,z1]
+                    ,[x2,y2,z2]
+                    ,[x3,y3,z3]]
+    in makeMatrix xs
+subColsX i
+  (Matrix [[x1,y1,z1], [x2,y2,z2]])
+  = let xs = case i of
+               0 -> [[y1,z1]
+                    ,[y2,z2]]
+               1 -> [[x1,z1]
+                    ,[x2,z2]]
+               2 -> [[x1,y1]
+                    ,[x2,y2]]
+    in makeMatrix xs
+  
+-- 'deletes' the given row and column from the given matrix
 submatrix :: Matrix -> RowIndex -> ColumnIndex -> Matrix
 submatrix (Matrix a) (RowIndex r) (ColumnIndex c)
   = let subRows = dropAt r a
-        subCols = map (dropAt c) subRows
-    in Matrix subCols
+        subCols = subColsX c (Matrix subRows)
+    in subCols
+
+test = submatrix (makeMatrix [[1,1,1],[2,2,2], [3,3,3]]) (RowIndex 0) (ColumnIndex 0)
+test2 = dropAt 0 [[1,1,1],[2,2,2], [3,3,3]]
 
 minor :: Matrix -> RowIndex -> ColumnIndex -> Double
 minor a r = determinant . submatrix a r
