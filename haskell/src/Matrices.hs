77476bb4048c4f27c5067c3fbcaf4c3af0bea0c1
Stefan Karlsson <info-s@tryne.se>
2022-05-30 07:44:09 +0200

    Linting

--- haskell/src/Matrices.hs
+++ haskell/src/Matrices.hs
@@ -175,16 +175,16 @@ mulV (VMatrix3x3
       (Vector3D a11 a12 a13) (Vector3D a21 a22 a23) (Vector3D a31 a32 a33))
      (VMatrix3x3
       (Vector3D b11 b12 b13) (Vector3D b21 b22 b23) (Vector3D b31 b32 b33))
-  = (VMatrix3x3
-     (Vector3D ((a11 * b11) + (a12 * b21) + (a13 * b31))
-               ((a11 * b12) + (a12 * b22) + (a13 * b32))
-               ((a11 * b13) + (a12 * b23) + (a13 * b33)))
-     (Vector3D ((a21 * b11) + (a22 * b21) + (a23 * b31))
-               ((a21 * b12) + (a22 * b22) + (a23 * b32))
-               ((a21 * b13) + (a22 * b23) + (a23 * b33)))
-     (Vector3D ((a31 * b11) + (a32 * b21) + (a33 * b31))
-               ((a31 * b12) + (a32 * b22) + (a33 * b32))
-               ((a31 * b13) + (a32 * b23) + (a33 * b33))))
+  = VMatrix3x3
+    (Vector3D ((a11 * b11) + (a12 * b21) + (a13 * b31))
+              ((a11 * b12) + (a12 * b22) + (a13 * b32))
+              ((a11 * b13) + (a12 * b23) + (a13 * b33)))
+    (Vector3D ((a21 * b11) + (a22 * b21) + (a23 * b31))
+              ((a21 * b12) + (a22 * b22) + (a23 * b32))
+              ((a21 * b13) + (a22 * b23) + (a23 * b33)))
+    (Vector3D ((a31 * b11) + (a32 * b21) + (a33 * b31))
+              ((a31 * b12) + (a32 * b22) + (a33 * b32))
+              ((a31 * b13) + (a32 * b23) + (a33 * b33)))
 
 mulV (VMatrix4x4
       (Vector4D a11 a12 a13 a14)
@@ -196,7 +196,7 @@ mulV (VMatrix4x4
       (Vector4D b21 b22 b23 b24)
       (Vector4D b31 b32 b33 b34)
       (Vector4D b41 b42 b43 b44))
-  = (VMatrix4x4
+  = VMatrix4x4
      (Vector4D ((a11 * b11) + (a12 * b21) + (a13 * b31) + (a14 * b41))
                ((a11 * b12) + (a12 * b22) + (a13 * b32) + (a14 * b42))
                ((a11 * b13) + (a12 * b23) + (a13 * b33) + (a14 * b43))
@@ -212,7 +212,7 @@ mulV (VMatrix4x4
      (Vector4D ((a41 * b11) + (a42 * b21) + (a43 * b31) + (a44 * b41))
                ((a41 * b12) + (a42 * b22) + (a43 * b32) + (a44 * b42))
                ((a41 * b13) + (a42 * b23) + (a43 * b33) + (a44 * b43))
-               ((a41 * b14) + (a42 * b24) + (a43 * b34) + (a44 * b44))))
+               ((a41 * b14) + (a42 * b24) + (a43 * b34) + (a44 * b44)))
 
 mulT :: Matrix -> T.Tuple -> T.Tuple
 mulT a@(Matrix m) b = let get = (\m r c -> getAt m (RowIndex r) (ColumnIndex c))
@@ -223,7 +223,7 @@ mulT a@(Matrix m) b = let get = (\m r c -> getAt m (RowIndex r) (ColumnIndex c))
 mulTV :: VMatrix -> T.Tuple -> T.Tuple
 mulTV (VMatrix4x4 a b c d) (T.Tuple x y z w)
   = let tv = Vector4D x y z w
-    in (T.Tuple (a `dot` tv) (b `dot` tv) (c `dot` tv) (d `dot` tv))
+    in T.Tuple (a `dot` tv) (b `dot` tv) (c `dot` tv) (d `dot` tv)
 
 transpose :: Matrix -> Matrix
 transpose a =
@@ -253,14 +253,14 @@ determinant m@(Matrix x) =
 determinantV :: VMatrix -> Double
 determinantV (VMatrix2x2 (Vector2D a b) (Vector2D c d)) =  a * d - c * b
 determinantV m@(VMatrix3x3 (Vector3D a b c) _ _)
-  =  (a * (cofactorV m (RowIndex 0) (ColumnIndex 0)))
-   + (b * (cofactorV m (RowIndex 0) (ColumnIndex 1)))
-   + (c * (cofactorV m (RowIndex 0) (ColumnIndex 2)))
+  =  (a * cofactorV m (RowIndex 0) (ColumnIndex 0))
+   + (b * cofactorV m (RowIndex 0) (ColumnIndex 1))
+   + (c * cofactorV m (RowIndex 0) (ColumnIndex 2))
 determinantV m@(VMatrix4x4 (Vector4D a b c d) _ _ _)
-  =  (a * (cofactorV m (RowIndex 0) (ColumnIndex 0)))
-   + (b * (cofactorV m (RowIndex 0) (ColumnIndex 1)))
-   + (c * (cofactorV m (RowIndex 0) (ColumnIndex 2)))
-   + (d * (cofactorV m (RowIndex 0) (ColumnIndex 3)))
+  =  (a * cofactorV m (RowIndex 0) (ColumnIndex 0))
+   + (b * cofactorV m (RowIndex 0) (ColumnIndex 1))
+   + (c * cofactorV m (RowIndex 0) (ColumnIndex 2))
+   + (d * cofactorV m (RowIndex 0) (ColumnIndex 3))
 
 subColsX :: Int -> Matrix -> Matrix
 subColsX i
