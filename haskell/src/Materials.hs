0be0bd9e26ba7917bd89e8ed71aa5a7c3aa3621a
Stefan Karlsson <info-s@tryne.se>
2022-07-20 12:06:26 +0200

    Add possible pattern for materials

--- haskell/src/Materials.hs
+++ haskell/src/Materials.hs
@@ -4,13 +4,15 @@ module Materials
   )where
 
 import Tuples
+import Patterns
 
 data Material = Material { color     :: Color
                          , ambient   :: Double
                          , diffuse   :: Double
                          , specular  :: Double
-                         , shininess :: Double }
+                         , shininess :: Double
+                         , pattern   :: Maybe Pattern}
                 deriving (Show, Eq, Ord)
 
 material :: Material
-material = Material (Color (Red 1) (Green 1) (Blue 1)) 0.1 0.9 0.9 200.0
+material = Material (Color (Red 1) (Green 1) (Blue 1)) 0.1 0.9 0.9 200.0 Nothing
