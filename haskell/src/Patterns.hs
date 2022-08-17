1b072dae9ca827017bc3e968295138b650770f96
Stefan Karlsson <info-s@tryne.se>
2022-08-17 07:27:11 +0200

    Linting

--- haskell/src/Patterns.hs
+++ haskell/src/Patterns.hs
@@ -43,15 +43,15 @@ gradientPattern :: Color -> Color -> Pattern
 gradientPattern aC bC = Pattern aC bC identityV Gradient
 
 gradientAt :: Pattern -> Tuple -> Color
-gradientAt gradient point = let distance = (b gradient) `subC` (a gradient)
-                                fraction = (x point) - (fromIntegral (floor (x point)))
-                            in (a gradient) `addC` (distance `mulCS` fraction)
+gradientAt gradient point = let distance = b gradient `subC` a gradient
+                                fraction = x point - fromIntegral (floor (x point))
+                            in a gradient `addC` distance `mulCS` fraction
 
 ringPattern :: Color -> Color -> Pattern
 ringPattern a b = Pattern a b identityV Ring
 
 ringAt :: Pattern -> Tuple -> Color
-ringAt ring point = if even (floor (sqrt ((x point)**2 + (z point)**2)))
+ringAt ring point = if even (floor (sqrt (x point ** 2 + z point ** 2)))
                     then a ring
                     else b ring
 
@@ -59,9 +59,9 @@ checkersPattern :: Color -> Color -> Pattern
 checkersPattern a b = Pattern a b identityV Checkers
 
 checkersAt :: Pattern -> Tuple -> Color
-checkersAt checkers (Tuple x y z _) = if even ((floor x) + (floor y) + (floor z))
-                                      then (a checkers)
-                                      else (b checkers)
+checkersAt checkers (Tuple x y z _) = if even (floor x + floor y + floor z)
+                                      then a checkers
+                                      else b checkers
 
 patternAt :: Pattern -> Tuple -> Color
 patternAt p@Pattern { patternShape = ps } point =
