2148ab07f6e302a74fee6c4002277b4f3139f4d7
Stefan Karlsson <info-s@tryne.se>
2022-09-17 08:21:30 +0200

    Add test for cylinder intersection hits

--- haskell/test/CylindersSpec.hs
+++ haskell/test/CylindersSpec.hs
@@ -42,3 +42,45 @@ cylinderIntersections =
           xs         = map (localIntersect c) rays
       it "all rays misses" $ do
         map length xs `shouldBe` [0, 0, 0]
+    {- Scenario Outline: A ray strikes a cylinder
+         Given cyl ← cylinder()
+           And direction ← normalize(<direction>)
+           And r ← ray(<origin>, direction)
+         When xs ← local_intersect(cyl, r)
+         Then xs.count = 2
+           And xs[0].t = <t0>
+           And xs[1].t = <t1>
+
+         Examples:
+           | origin            | direction         | t0      | t1      |
+           | point(1, 0, -5)   | vector(0, 0, 1)   | 5       | 5       |
+           | point(0, 0, -5)   | vector(0, 0, 1)   | 4       | 6       |
+           | point(0.5, 0, -5) | vector(0.1, 1, 1) | 6.80798 | 7.08872 | -}
+    describe "A ray strikes a cylinder" $ do
+      let cyl        = defaultCylinder 1
+          origins    = [ T.point 1 0 (-5)
+                       , T.point 0 0 (-5)
+                       , T.point 0.5 0 (-5)]
+          directions = map T.norm [ T.vector 0 0 1
+                                  , T.vector 0 0 1
+                                  , T.vector 0.1 1 1]
+          rays       = zipWith makeRay origins directions
+          t0s        = [5, 4, 6.80798]
+          t1s        = [5, 6, 7.08872]
+          xs         = map (localIntersect cyl) rays
+          getT0 [(Intersection t _), _] = t
+          getT1 [_, (Intersection t _)] = t
+      it "all ray hits" $ do
+        map length xs `shouldBe` [2, 2, 2]
+      it "t0 - 1 intersect" $ do
+        getT0 (xs !! 0) `shouldBe` t0s !! 0
+      it "t0 - 2 intersect" $ do
+        getT0 (xs !! 1) `shouldBe` t0s !! 1
+      it "t0 - 3 intersect" $ do
+        getT0 (xs !! 2) ~= (t0s !! 2) `shouldBe` True
+      it "t1 - 1 intersect" $ do
+        getT1 (xs !! 0) `shouldBe` t1s !! 0
+      it "t1 - 2 intersect" $ do
+        getT1 (xs !! 1) `shouldBe` t1s !! 1
+      it "t1 - 3 intersect" $ do
+        getT1 (xs !! 2) ~= (t1s !! 2) `shouldBe` True
