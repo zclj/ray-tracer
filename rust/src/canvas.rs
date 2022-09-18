f0ae1baa17b416cd17079e499057168eebf7a9d1
Stefan <info-s@tryne.se>
2022-09-18 17:23:13 +0200

    Implement write_at at pixel_at for Canvas

--- rust/src/canvas.rs
+++ rust/src/canvas.rs
@@ -14,6 +14,16 @@ impl Canvas {
             pixels: vec![Color::new(0.0, 0.0, 0.0); (width * height).into()],
         }
     }
+
+    fn write_pixel(&mut self, width: u16, height: u16, color: Color) {
+        let idx: usize = (width * height).into();
+        self.pixels[idx] = color
+    }
+
+    fn pixel_at(&self, width: u16, height: u16) -> &Color {
+        let idx: usize = (width * height).into();
+        &self.pixels[idx]
+    }
 }
 
 mod test {
@@ -33,5 +43,20 @@ mod test {
         assert_eq!(c.width, 10);
         assert_eq!(c.height, 20);
         assert_eq!(c.pixels.iter().all(|c| *c == black), true);
+    }
+
+    // Scenario: Writing pixels to a canvas
+    // Given c ← canvas(10, 20)
+    //   And red ← color(1, 0, 0)
+    // When write_pixel(c, 2, 3, red)
+    // Then pixel_at(c, 2, 3) = red
+    #[test]
+    fn writing_pixels_to_a_canvas() {
+        let mut c = Canvas::new(10, 20);
+        let red = Color::new(1.0, 0.0, 0.0);
+
+        c.write_pixel(2, 3, red);
+
+        assert_eq!(*c.pixel_at(2, 3), Color::new(1.0, 0.0, 0.0))
     }
 }
