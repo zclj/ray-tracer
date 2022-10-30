03c28c8c17ac4d78e43f77ff86f5a4253e5b440d
Stefan <info-s@tryne.se>
2022-10-30 11:10:25 +0100

    Add benchmark for canvas to ppm

--- rust/benches/raymark.rs
+++ rust/benches/raymark.rs
@@ -2,7 +2,9 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ray_tracer::canvas::Canvas;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("projectile", |b| b.iter(|| Canvas::new(black_box(10),black_box(10))));
+    let canvas = Canvas::new(black_box(900), black_box(600));
+
+    c.bench_function("canvas to ppm", |b| b.iter(|| canvas.to_ppm()));
 }
 
 criterion_group!(benches, criterion_benchmark);
