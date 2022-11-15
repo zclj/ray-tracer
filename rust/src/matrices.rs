bb2ae94d461abd8eb6694f3dfefae2104d6ad694
Stefan <info-s@tryne.se>
2022-11-15 18:32:31 +0100

    Implement transpose 4x4

--- rust/src/matrices.rs
+++ rust/src/matrices.rs
@@ -27,8 +27,14 @@ impl M4x4 {
         ])
     }
 
+    #[rustfmt::skip]
     pub fn transpose(&self) -> Self {
-        M4x4::IDENTITY
+        M4x4([
+            self[(0,0)], self[(1,0)], self[(2,0)], self[(3,0)],
+            self[(0,1)], self[(1,1)], self[(2,1)], self[(3,1)],
+            self[(0,2)], self[(1,2)], self[(2,2)], self[(3,2)],
+            self[(0,3)], self[(1,3)], self[(2,3)], self[(3,3)],
+        ])
     }
 }
 
