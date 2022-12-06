6370ee776d97733e722538fb09f4cd9997491ba8
Stefan <info-s@tryne.se>
2022-12-06 19:08:24 +0100

    Add benchmark of 4x4M Transpose

--- rust/benches/raymark.rs
+++ rust/benches/raymark.rs
@@ -98,12 +98,42 @@ pub fn matrix_assign_multiplication(c: &mut Criterion) {
     });
 }
 
+pub fn matrix_transpose(c: &mut Criterion) {
+    let a = M4x4::from_elements(
+        [0.0, 9.0, 3.0, 0.0],
+        [9.0, 8.0, 0.0, 8.0],
+        [1.0, 8.0, 5.0, 3.0],
+        [0.0, 0.0, 5.0, 8.0],
+    );
+
+    c.bench_function("Matrix 4x4 Transpose", |b| {
+        b.iter(|| black_box(a.transpose()))
+    });
+}
+
+pub fn glam_matrix_transpose(c: &mut Criterion) {
+    let x = Mat4::from_cols_array_2d(&[
+        [0.0, 9.0, 3.0, 0.0],
+        [9.0, 8.0, 0.0, 8.0],
+        [1.0, 8.0, 5.0, 3.0],
+        [0.0, 0.0, 5.0, 8.0],
+    ]);
+
+    c.bench_function("glam Matrix 4x4 Transpose", |b| {
+        b.iter(|| black_box(x.transpose()))
+    });
+}
+
+// TODO : submatrix, minor, cofactor, determinant, inverse, Eq, Indexing
+
 criterion_group!(
     benches,
     criterion_benchmark,
     canvas_colored,
     matrix_multiplication,
     glam_matrix_multiplication,
-    matrix_assign_multiplication //push_three_digits
+    matrix_assign_multiplication,
+    matrix_transpose,
+    glam_matrix_transpose,
 );
 criterion_main!(benches);
