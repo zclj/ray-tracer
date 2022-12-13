10e0145c6c0d360af386905df463a256a176e356
Stefan <info-s@tryne.se>
2022-12-13 19:38:24 +0100

    Finish basic Matrix benches

--- rust/benches/raymark.rs
+++ rust/benches/raymark.rs
@@ -124,6 +124,7 @@ pub fn glam_matrix_transpose(c: &mut Criterion) {
     });
 }
 
+// @NOTE - don't find any glam correspondence to sub matrix
 pub fn matrix_sub_matrix(c: &mut Criterion) {
     let a = M4x4::from_elements(
         [-6.0, 1.0, 1.0, 6.0],
@@ -162,9 +163,77 @@ pub fn glam_matrix_determinant(c: &mut Criterion) {
         b.iter(|| black_box(x.determinant()))
     });
 }
-// @NOTE - don't find any glam correspondence to sub matrix
 
-// TODO : inverse, Eq, Indexing
+pub fn matrix_inverse(c: &mut Criterion) {
+    let a = M4x4::from_elements(
+        [0.0, 9.0, 3.0, 0.0],
+        [9.0, 8.0, 0.0, 8.0],
+        [1.0, 8.0, 5.0, 3.0],
+        [0.0, 0.0, 5.0, 8.0],
+    );
+
+    c.bench_function("Matrix 4x4 Inverse", |b| b.iter(|| black_box(a.inverse())));
+}
+
+pub fn glam_matrix_inverse(c: &mut Criterion) {
+    let x = Mat4::from_cols_array_2d(&[
+        [0.0, 9.0, 3.0, 0.0],
+        [9.0, 8.0, 0.0, 8.0],
+        [1.0, 8.0, 5.0, 3.0],
+        [0.0, 0.0, 5.0, 8.0],
+    ]);
+
+    c.bench_function("glam Matrix 4x4 Inverse", |b| {
+        b.iter(|| black_box(x.inverse()))
+    });
+}
+
+pub fn matrix_equality(c: &mut Criterion) {
+    let x = M4x4::from_elements(
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 8.0, 7.0, 6.0],
+        [5.0, 4.0, 3.0, 2.0],
+    );
+
+    let y = M4x4::from_elements(
+        [2.0, 3.0, 4.0, 5.0],
+        [6.0, 7.0, 8.0, 9.0],
+        [8.0, 7.0, 6.0, 5.0],
+        [4.0, 3.0, 2.0, 1.0],
+    );
+
+    c.bench_function("Matrix 4x4 Equality", |b| b.iter(|| black_box(x == y)));
+}
+
+pub fn glam_matrix_equality(c: &mut Criterion) {
+    let x = Mat4::from_cols_array_2d(&[
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 8.0, 7.0, 6.0],
+        [5.0, 4.0, 3.0, 2.0],
+    ]);
+
+    let y = Mat4::from_cols_array_2d(&[
+        [2.0, 3.0, 4.0, 5.0],
+        [6.0, 7.0, 8.0, 9.0],
+        [8.0, 7.0, 6.0, 5.0],
+        [4.0, 3.0, 2.0, 1.0],
+    ]);
+
+    c.bench_function("glam Matrix 4x4 Equality", |b| b.iter(|| black_box(x == y)));
+}
+
+pub fn matrix_indexing(c: &mut Criterion) {
+    let x = M4x4::from_elements(
+        [1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [9.0, 8.0, 7.0, 6.0],
+        [5.0, 4.0, 3.0, 2.0],
+    );
+
+    c.bench_function("Matrix 4x4 Indexing", |b| b.iter(|| black_box(x[(2, 2)])));
+}
 
 criterion_group!(
     benches,
@@ -178,5 +247,10 @@ criterion_group!(
     matrix_sub_matrix,
     matrix_determinant,
     glam_matrix_determinant,
+    matrix_inverse,
+    glam_matrix_inverse,
+    matrix_equality,
+    glam_matrix_equality,
+    matrix_indexing,
 );
 criterion_main!(benches);
