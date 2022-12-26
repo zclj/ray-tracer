908a259b1e4016caa2c0d5034fc570b2f896627a
Stefan <info-s@tryne.se>
2022-12-26 17:01:18 +0100

    Add more sphere/ray intersection tests

--- rust/src/spheres.rs
+++ rust/src/spheres.rs
@@ -53,4 +53,72 @@ mod test {
         assert_eq!(xs[0], 4.0);
         assert_eq!(xs[1], 6.0);
     }
+
+    // Scenario: A ray intersects a sphere at a tangent
+    //   Given r ← ray(point(0, 1, -5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = 5.0
+    //     And xs[1] = 5.0
+    #[test]
+    fn a_ray_intersects_a_sphere_at_a_tangent() {
+        let r = Ray::new(Point::new(0.0, 1.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new(1);
+        let xs = intersect(&s, &r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], 5.0);
+        assert_eq!(xs[1], 5.0);
+    }
+
+    // Scenario: A ray misses a sphere
+    //   Given r ← ray(point(0, 2, -5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 0
+    #[test]
+    fn a_ray_misses_a_sphere() {
+        let r = Ray::new(Point::new(0.0, 2.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new(1);
+        let xs = intersect(&s, &r);
+
+        assert_eq!(xs.len(), 0);
+    }
+
+    // Scenario: A ray originates inside a sphere
+    //   Given r ← ray(point(0, 0, 0), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = -1.0
+    //     And xs[1] = 1.0
+    #[test]
+    fn a_ray_originates_inside_a_sphere() {
+        let r = Ray::new(Point::new(0.0, 0.0, 0.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new(1);
+        let xs = intersect(&s, &r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], -1.0);
+        assert_eq!(xs[1], 1.0);
+    }
+
+    // Scenario: A sphere is behind a ray
+    //   Given r ← ray(point(0, 0, 5), vector(0, 0, 1))
+    //     And s ← sphere()
+    //   When xs ← intersect(s, r)
+    //   Then xs.count = 2
+    //     And xs[0] = -6.0
+    //     And xs[1] = -4.0
+    #[test]
+    fn a_sphere_is_behind_a_ray() {
+        let r = Ray::new(Point::new(0.0, 0.0, 5.0), Vector::new(0.0, 0.0, 1.0));
+        let s = Sphere::new(1);
+        let xs = intersect(&s, &r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0], -6.0);
+        assert_eq!(xs[1], -4.0);
+    }
 }
