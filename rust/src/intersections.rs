9c666997304d566a91739147b831654db502b64f
Stefan <info-s@tryne.se>
2022-12-28 17:17:06 +0100

    Implement hit function

--- rust/src/intersections.rs
+++ rust/src/intersections.rs
@@ -1,7 +1,8 @@
 use crate::rays::Ray;
 use crate::shape::Shape;
-use crate::vector::{Point};
+use crate::vector::Point;
 
+#[derive(Debug, PartialEq)]
 pub struct Intersection {
     pub t: f32,
     pub object: u32,
@@ -48,10 +49,22 @@ fn intersect(shape: &Shape, r: &Ray) -> Vec<Intersection> {
     ]
 }
 
+fn hit(xs: &mut [Intersection]) -> Option<&Intersection> {
+    xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+    let pos_xs: Vec<&Intersection> = xs.iter().filter(|x| x.t >= 0.0).collect();
+
+    match pos_xs.first() {
+        Some(i) => Some(*i),
+        _ => None,
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::context::Context;
     use crate::shape::Shape;
+    use crate::vector::Vector;
 
     // Scenario: An intersection encapsulates t and object
     //   Given s ← sphere()
@@ -202,5 +215,89 @@ mod test {
         assert_eq!(xs.len(), 2);
         assert_eq!(xs[0].object, s_id);
         assert_eq!(xs[1].object, s_id);
+    }
+
+    // Scenario: The hit, when all intersections have positive t
+    //   Given s ← sphere()
+    //     And i1 ← intersection(1, s)
+    //     And i2 ← intersection(2, s)
+    //     And xs ← intersections(i2, i1)
+    //   When i ← hit(xs)
+    //   Then i = i1
+    #[test]
+    fn the_hit_when_all_intersections_have_positive_t() {
+        let mut ctx = Context::new();
+        let s_id = ctx.push_sphere();
+        let i1 = Intersection::new(1.0, s_id);
+        let i2 = Intersection::new(2.0, s_id);
+
+        let mut is = [i1, i2];
+        let i = hit(&mut is);
+
+        assert_eq!(i, Some(&Intersection::new(1.0, s_id)));
+    }
+
+    // Scenario: The hit, when some intersections have negative t
+    //   Given s ← sphere()
+    //     And i1 ← intersection(-1, s)
+    //     And i2 ← intersection(1, s)
+    //     And xs ← intersections(i2, i1)
+    //   When i ← hit(xs)
+    //   Then i = i2
+    #[test]
+    fn the_hit_when_some_intersections_have_negative_t() {
+        let mut ctx = Context::new();
+        let s_id = ctx.push_sphere();
+        let i1 = Intersection::new(-1.0, s_id);
+        let i2 = Intersection::new(1.0, s_id);
+
+        let mut is = [i2, i1];
+        let i = hit(&mut is);
+
+        assert_eq!(i, Some(&Intersection::new(1.0, s_id)));
+    }
+
+    // Scenario: The hit, when all intersections have negative t
+    //   Given s ← sphere()
+    //     And i1 ← intersection(-2, s)
+    //     And i2 ← intersection(-1, s)
+    //     And xs ← intersections(i2, i1)
+    //   When i ← hit(xs)
+    //   Then i is nothing
+    #[test]
+    fn the_hit_when_all_intersections_have_negative_t() {
+        let mut ctx = Context::new();
+        let s_id = ctx.push_sphere();
+        let i1 = Intersection::new(-2.0, s_id);
+        let i2 = Intersection::new(-1.0, s_id);
+
+        let mut is = [i2, i1];
+        let i = hit(&mut is);
+
+        assert_eq!(i, None);
+    }
+
+    // Scenario: The hit is always the lowest nonnegative intersection
+    //   Given s ← sphere()
+    //   And i1 ← intersection(5, s)
+    //   And i2 ← intersection(7, s)
+    //   And i3 ← intersection(-3, s)
+    //   And i4 ← intersection(2, s)
+    //   And xs ← intersections(i1, i2, i3, i4)
+    // When i ← hit(xs)
+    // Then i = i4
+    #[test]
+    fn the_hit_is_always_the_lowest_nonnegative_intersection() {
+        let mut ctx = Context::new();
+        let s_id = ctx.push_sphere();
+        let i1 = Intersection::new(5.0, s_id);
+        let i2 = Intersection::new(7.0, s_id);
+        let i3 = Intersection::new(-3.0, s_id);
+        let i4 = Intersection::new(2.0, s_id);
+
+        let mut is = [i1, i2, i3, i4];
+        let i = hit(&mut is);
+
+        assert_eq!(i, Some(&Intersection::new(2.0, s_id)));
     }
 }
