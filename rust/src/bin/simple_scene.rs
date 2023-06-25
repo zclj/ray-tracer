c46b22b8e1da25fbac03d247c5156d675911e0ed
Stefan <info-s@tryne.se>
2023-06-25 16:21:55 +0200

    Obsolete demo

--- rust/src/bin/simple_scene.rs
+++ rust/src/bin/simple_scene.rs
@@ -15,114 +15,115 @@ use std::path::Path;
 fn main() {
     println!("Simple scene with shadows - demo");
 
-    let mut world = World::new();
+    println!("Obsolete");
+    // let mut world = World::new();
 
-    world.light = PointLight {
-        position: Point::new(-10.0, 10.0, -10.0),
-        intensity: Color::new(1.0, 1.0, 1.0),
-    };
+    // world.light = PointLight {
+    //     position: Point::new(-10.0, 10.0, -10.0),
+    //     intensity: Color::new(1.0, 1.0, 1.0),
+    // };
 
-    world.shadow_bias = 0.01;
+    // world.shadow_bias = 0.01;
 
-    // shapes
-    let _floor_id = world.push_sphere(
-        Some(scaling(10.0, 0.01, 10.0)),
-        Some(Material {
-            color: Color::new(1.0, 0.9, 0.9),
-            specular: 0.0,
-            ..Material::default()
-        }),
-    );
+    // // shapes
+    // let _floor_id = world.push_sphere(
+    //     Some(scaling(10.0, 0.01, 10.0)),
+    //     Some(Material {
+    //         color: Color::new(1.0, 0.9, 0.9),
+    //         specular: 0.0,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let _left_wall_id = world.push_sphere(
-        Some(transform(&[
-            scaling(10.0, 0.01, 10.0),
-            rotation_x(PI / 2.0),
-            rotation_y(-PI / 4.0),
-            translation(0.0, 0.0, 5.0),
-        ])),
-        Some(Material {
-            color: Color::new(1.0, 0.9, 0.9),
-            specular: 0.0,
-            ..Material::default()
-        }),
-    );
+    // let _left_wall_id = world.push_sphere(
+    //     Some(transform(&[
+    //         scaling(10.0, 0.01, 10.0),
+    //         rotation_x(PI / 2.0),
+    //         rotation_y(-PI / 4.0),
+    //         translation(0.0, 0.0, 5.0),
+    //     ])),
+    //     Some(Material {
+    //         color: Color::new(1.0, 0.9, 0.9),
+    //         specular: 0.0,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let _right_wall_id = world.push_sphere(
-        Some(transform(&[
-            scaling(10.0, 0.01, 10.0),
-            rotation_x(PI / 2.0),
-            rotation_y(PI / 4.0),
-            translation(0.0, 0.0, 5.0),
-        ])),
-        Some(Material {
-            color: Color::new(1.0, 0.9, 0.9),
-            specular: 0.0,
-            ..Material::default()
-        }),
-    );
+    // let _right_wall_id = world.push_sphere(
+    //     Some(transform(&[
+    //         scaling(10.0, 0.01, 10.0),
+    //         rotation_x(PI / 2.0),
+    //         rotation_y(PI / 4.0),
+    //         translation(0.0, 0.0, 5.0),
+    //     ])),
+    //     Some(Material {
+    //         color: Color::new(1.0, 0.9, 0.9),
+    //         specular: 0.0,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let _middle_sphere_id = world.push_sphere(
-        Some(translation(-0.5, 1.0, 0.5)),
-        Some(Material {
-            color: Color::new(0.1, 1.0, 0.5),
-            diffuse: 0.7,
-            specular: 0.3,
-            ..Material::default()
-        }),
-    );
+    // let _middle_sphere_id = world.push_sphere(
+    //     Some(translation(-0.5, 1.0, 0.5)),
+    //     Some(Material {
+    //         color: Color::new(0.1, 1.0, 0.5),
+    //         diffuse: 0.7,
+    //         specular: 0.3,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let _right_sphere_id = world.push_sphere(
-        Some(transform(&[
-            scaling(0.5, 0.5, 0.5),
-            translation(1.5, 0.5, -0.5),
-        ])),
-        Some(Material {
-            color: Color::new(0.5, 1.0, 0.1),
-            diffuse: 0.7,
-            specular: 0.3,
-            ..Material::default()
-        }),
-    );
+    // let _right_sphere_id = world.push_sphere(
+    //     Some(transform(&[
+    //         scaling(0.5, 0.5, 0.5),
+    //         translation(1.5, 0.5, -0.5),
+    //     ])),
+    //     Some(Material {
+    //         color: Color::new(0.5, 1.0, 0.1),
+    //         diffuse: 0.7,
+    //         specular: 0.3,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let _left_sphere_id = world.push_sphere(
-        Some(transform(&[
-            scaling(0.33, 0.33, 0.33),
-            translation(-1.5, 0.33, -0.75),
-        ])),
-        Some(Material {
-            color: Color::new(1.0, 0.8, 0.1),
-            diffuse: 0.7,
-            specular: 0.3,
-            ..Material::default()
-        }),
-    );
+    // let _left_sphere_id = world.push_sphere(
+    //     Some(transform(&[
+    //         scaling(0.33, 0.33, 0.33),
+    //         translation(-1.5, 0.33, -0.75),
+    //     ])),
+    //     Some(Material {
+    //         color: Color::new(1.0, 0.8, 0.1),
+    //         diffuse: 0.7,
+    //         specular: 0.3,
+    //         ..Material::default()
+    //     }),
+    // );
 
-    let camera = Camera::new(
-        1000,
-        500,
-        PI / 3.0,
-        &view_transform(
-            &Point::new(0.1, 1.5, -7.0),
-            &Point::new(0.0, 1.0, 0.0),
-            &Vector::new(0.0, 1.0, 0.0),
-        ),
-    );
+    // let camera = Camera::new(
+    //     1000,
+    //     500,
+    //     PI / 3.0,
+    //     &view_transform(
+    //         &Point::new(0.1, 1.5, -7.0),
+    //         &Point::new(0.0, 1.0, 0.0),
+    //         &Vector::new(0.0, 1.0, 0.0),
+    //     ),
+    // );
 
-    let canvas = camera.render(&world, 1);
+    // let canvas = camera.render(&world, 1);
 
-    let ppm = canvas.to_ppm();
+    // let ppm = canvas.to_ppm();
 
-    let path = Path::new("demos/ppms/simple_scene.ppm");
+    // let path = Path::new("demos/ppms/simple_scene.ppm");
 
-    let mut file = match File::create(path) {
-        Err(why) => panic!("could not create file {}: {} ", path.display(), why),
-        Ok(file) => file,
-    };
+    // let mut file = match File::create(path) {
+    //     Err(why) => panic!("could not create file {}: {} ", path.display(), why),
+    //     Ok(file) => file,
+    // };
 
-    if let Err(why) = file.write_all(ppm.as_bytes()) {
-        panic!("could not write file {}: {} ", path.display(), why)
-    }
+    // if let Err(why) = file.write_all(ppm.as_bytes()) {
+    //     panic!("could not write file {}: {} ", path.display(), why)
+    // }
 
     println!("Scene Done!");
 }
