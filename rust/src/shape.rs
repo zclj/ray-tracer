5d4a3bb989c47fdfad0e9e4c0e0bd25cea99b368
Stefan <info-s@tryne.se>
2023-12-27 10:05:09 +0100

    Add bounding box to triangle

--- rust/src/shape.rs
+++ rust/src/shape.rs
@@ -266,7 +266,13 @@ impl RenderObject {
 #[must_use]
 pub fn bounds(kind: &Shape) -> BoundingBox {
     match kind {
-        Shape::Triangle { .. } => todo!(),
+        Shape::Triangle { p1, p2, p3, .. } => {
+            let mut bb = BoundingBox::default();
+            bb.add_point(p1);
+            bb.add_point(p2);
+            bb.add_point(p3);
+            bb
+        }
         Shape::Sphere | Shape::Cube => {
             BoundingBox::new(Point::new(-1.0, -1.0, -1.0), Point::new(1.0, 1.0, 1.0))
         }
@@ -711,6 +717,33 @@ mod test {
         assert_eq!(
             bounds(&world.get_object(0).kind),
             BoundingBox::new(Point::new(-5.0, -5.0, -5.0), Point::new(5.0, 3.0, 5.0))
+        )
+    }
+
+    // Scenario: A triangle has a bounding box
+    //   Given p1 ← point(-3, 7, 2)
+    //     And p2 ← point(6, 2, -4)
+    //     And p3 ← point(2, -1, -1)
+    //     And shape ← triangle(p1, p2, p3)
+    //   When box ← bounds_of(shape)
+    //     And box.max = point(6, 7, 2)
+    //   Then box.min = point(-3, -1, -4)
+    #[test]
+    fn a_triangle_has_a_bounding_box() {
+        let mut world = World::new();
+
+        let p1 = Point::new(-3.0, 7.0, 2.0);
+        let p2 = Point::new(6.0, 2.0, -4.0);
+        let p3 = Point::new(2.0, -1.0, -1.0);
+
+        let s = Shape::new_triangle(p1, p2, p3);
+
+        let _s_id = world.scene.insert_object(s, None, None);
+        world.build();
+
+        assert_eq!(
+            bounds(&world.get_object(0).kind),
+            BoundingBox::new(Point::new(-3.0, -1.0, -4.0), Point::new(6.0, 7.0, 2.0))
         )
     }
 
@@ -1208,5 +1241,40 @@ mod test {
             }
             _ => panic!("Wrong shape"),
         }
+    }
+
+    // Scenario: Finding the normal on a triangle
+    //   Given t ← triangle(point(0, 1, 0), point(-1, 0, 0), point(1, 0, 0))
+    //   When n1 ← local_normal_at(t, point(0, 0.5, 0))
+    //     And n2 ← local_normal_at(t, point(-0.5, 0.75, 0))
+    //     And n3 ← local_normal_at(t, point(0.5, 0.25, 0))
+    //   Then n1 = t.normal
+    //     And n2 = t.normal
+    //     And n3 = t.normal
+    #[test]
+    fn finding_the_normal_on_a_triangle() {
+        let mut world = World::new();
+
+        let p1 = Point::new(0.0, 1.0, 0.0);
+        let p2 = Point::new(-1.0, 0.0, 0.0);
+        let p3 = Point::new(1.0, 0.0, 0.0);
+
+        let t = Shape::new_triangle(p1, p2, p3);
+        let expected_normal = match &t {
+            Shape::Triangle { normal, .. } => normal.clone(),
+            _ => panic!("Wrong shape"),
+        };
+
+        let s_id = world.scene.insert_object(t, None, None);
+        world.build();
+        let s = world.get_object(s_id);
+
+        let n1 = s.normal_at(&Point::new(0.0, 0.5, 0.0));
+        let n2 = s.normal_at(&Point::new(-0.5, 0.75, 0.0));
+        let n3 = s.normal_at(&Point::new(0.5, 0.25, 0.0));
+
+        assert_eq!(n1, expected_normal);
+        assert_eq!(n2, expected_normal);
+        assert_eq!(n3, expected_normal);
     }
 }
