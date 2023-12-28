3f7624838eea5922ebbcd58e505e1dd9f85851db
Stefan <info-s@tryne.se>
2023-12-28 09:54:42 +0100

    Add basic group obj support

--- rust/src/obj_file_parser.rs
+++ rust/src/obj_file_parser.rs
@@ -27,6 +27,9 @@ pub fn parse(content: &str) -> ParseResult {
     let mut result = ParseResult::new();
     let mut default_group_ids = vec![];
 
+    let mut current_group_ids: Option<Vec<u32>> = None;
+    //let mut current_group_id = None;
+
     for l in content.lines() {
         if l.is_empty() {
             continue;
@@ -48,10 +51,10 @@ pub fn parse(content: &str) -> ParseResult {
                 }
                 "f" => {
                     let idxs = parts.collect::<Vec<&str>>();
-                    
+
                     for idx_str in 1..(idxs.len() - 1) {
-                        let idx:usize = idxs[idx_str].parse().unwrap();
-                        
+                        let idx: usize = idxs[idx_str].parse().unwrap();
+
                         let p1 = result.vertices[0].clone();
                         let p2 = result.vertices[idx - 1].clone();
                         let p3 = result.vertices[idx].clone();
@@ -62,7 +65,24 @@ pub fn parse(content: &str) -> ParseResult {
                             None,
                         );
 
-                        default_group_ids.push(s_id);
+                        if let Some(ref mut current_group) = current_group_ids {
+                            current_group.push(s_id)
+                        } else {
+                            default_group_ids.push(s_id);
+                        }
+                    }
+                }
+                "g" => {
+                    let _group_parts = parts.collect::<Vec<&str>>();
+                    // set a new current group
+                    if let Some(current_group) = current_group_ids {
+                        let g_id = result.world.scene.insert_group(current_group, None);
+
+                        default_group_ids.push(g_id);
+                        current_group_ids = Some(vec![]);
+                    } else {
+                        // no active group
+                        current_group_ids = Some(vec![]);
                     }
                 }
                 _ => result.ignored.push(line.to_string()),
@@ -70,6 +90,13 @@ pub fn parse(content: &str) -> ParseResult {
         } else {
             result.ignored.push(line.to_string())
         }
+    }
+
+    // If there is an active named group, add it
+    if let Some(current_group) = current_group_ids {
+        let g_id = result.world.scene.insert_group(current_group, None);
+
+        default_group_ids.push(g_id);
     }
 
     let g_id = result.world.scene.insert_group(default_group_ids, None);
@@ -213,7 +240,7 @@ mod tests {
     //     v 1 0 0
     //     v 1 1 0
     //     v 0 2 0
-    
+
     //     f 1 2 3 4 5
     //     """
     //   When parser ← parse_obj_file(file)
@@ -248,7 +275,6 @@ mod tests {
 
         println!("World: {:#?}", res.world);
 
-        let group = &res.world.scene.arena[g_id as usize];
         println!("Group {:#?}", res.world.scene.arena[g_id as usize]);
 
         match &res.world.get_object(0).kind {
@@ -282,4 +308,36 @@ mod tests {
         assert_eq!(0, res.ignored.len())
     }
 
+    // Scenario: Triangles in groups
+    //   Given file ← the file "triangles.obj"
+    //   When parser ← parse_obj_file(file)
+    //     And g1 ← "FirstGroup" from parser
+    //     And g2 ← "SecondGroup" from parser
+    //     And t1 ← first child of g1
+    //     And t2 ← first child of g2
+    //   Then t1.p1 = parser.vertices[1]
+    //     And t1.p2 = parser.vertices[2]
+    //     And t1.p3 = parser.vertices[3]
+    //     And t2.p1 = parser.vertices[1]
+    //     And t2.p2 = parser.vertices[3]
+    //     And t2.p3 = parser.vertices[4]
+    #[test]
+    fn triangles_in_groups() {
+        let content = "
+            v -1 1 0
+            v -1 0 0
+            v 1 0 0
+            v 1 1 0
+
+            g FirstGroup
+            f 1 2 3
+            g SecondGroup
+            f 1 3 4";
+
+        let mut res = parse(content);
+        res.world.build();
+
+        assert_eq!(4, res.vertices.len());
+        assert_eq!(0, res.ignored.len())
+    }
 }
